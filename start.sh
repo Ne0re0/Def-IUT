35212094dbb789f8fdff546fb30345edfabaaaa3
Ne0re0 <neo.leborgne@gmail.com>
2024-01-04 19:47:35 +0100

    Add start and stop

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ./src/express_webapp
+npm start > /dev/null 2>/dev/null &
+cd ../../
+echo "Service démarré sur le port 3000 : http://localhost:3000"
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pkill -9 'node'
+pkill -9 "npm start"
+echo "Service arrêté"
